034b2907d712980df7583a96004a61099c850843
Thanasis-Kottaridis <thanasiskottaridis@yahoo.gr>
2022-09-18 16:01:37 +0300

    Implementation: Configure 2 layers LSTM wit 34.170 RMSE

--- cryptoDataPrediction/crypto_prediction_modeling.py
+++ cryptoDataPrediction/crypto_prediction_modeling.py
@@ -16,36 +16,47 @@ from tensorflow.keras.layers import Dense, LSTM, Dropout, BatchNormalization
 import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 
+# Check GPU
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print(device_lib.list_local_devices())
+
 # Constants
+DATA_FILE_NAME = 'final_crypto_data.csv'
+readFromMongo = False
 isDemoMode = True
 doPlots = True
+loadModel = False
 
-def toMultivariateSeries(input_sequences, output_sequence, n_steps_in, n_steps_out) :
+
+def toMultivariateSeries(input_sequences, output_sequence, n_steps_in, n_steps_out):
     """
         Split timeSeries
         This helper function is used in order to split dataset in time series.
         split a multivariate sequence past, future samples (X and y)
     """
     X_list, y_list = list(), list()  # instantiate X and y
-    for i in range(len(input_sequences)) :
+    for i in range(len(input_sequences)):
         # find the end of the input, output sequence
         end_ix = i + n_steps_in
         out_end_ix = end_ix + n_steps_out - 1
         # check if we are beyond the dataset
-        if out_end_ix > len(input_sequences) : break
+        if out_end_ix > len(input_sequences): break
         # gather input and output of the pattern
-        seq_x, seq_y = input_sequences[i :end_ix], output_sequence[end_ix - 1 :out_end_ix, -1]
+        seq_x, seq_y = input_sequences[i:end_ix], output_sequence[end_ix - 1:out_end_ix, -1]
         X_list.append(seq_x), y_list.append(seq_y)
     return np.array(X_list), np.array(y_list)
 
 
-def train_test_valid_split(total_x, total_y, train_size=0.9, valid_size=0.1) :
+def train_test_valid_split(total_x, total_y, train_size=0.9, valid_size=0.1):
     train_index = int(len(total_x) * train_size)
     valid_index = int(len(total_x) * valid_size)
 
-    X_train, y_train = total_x[0 :train_index], total_y[0 :train_index]
-    X_valid, y_valid = total_x[train_index :train_index + valid_index], total_y[train_index :train_index + valid_index]
-    X_test, y_test = total_x[train_index + valid_index :], total_y[train_index + valid_index :]
+    X_train, y_train = total_x[0:train_index], total_y[0:train_index]
+    X_valid, y_valid = total_x[train_index:train_index + valid_index], total_y[train_index:train_index + valid_index]
+    X_test, y_test = total_x[train_index + valid_index:], total_y[train_index + valid_index:]
 
     print("-------- train test valid split --------")
     print(len(X_train)), print(len(y_train))
@@ -61,7 +72,7 @@ def train_test_valid_split(total_x, total_y, train_size=0.9, valid_size=0.1) :
            np.array(y_test)
 
 
-def getModelCheckpointCallback(checkpoint_path) :
+def getModelCheckpointCallback(checkpoint_path):
     checkpoint_dir = os.path.dirname(checkpoint_path)
     # Create a callback that saves the model's weights
     return tf.keras.callbacks.ModelCheckpoint(
@@ -71,8 +82,7 @@ def getModelCheckpointCallback(checkpoint_path) :
     )
 
 
-def display_training_curves(training, validation, title, subplot=None) :
-
+def display_training_curves(training, validation, title, subplot=None):
     if not doPlots:
         return
     if subplot is not None:
@@ -89,34 +99,37 @@ def display_training_curves(training, validation, title, subplot=None) :
     return
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     # Get Processed Crypto Data
     # - Get Data from mongo
     # - store data in DF
 
-    # connecting or switching to the database
-    connection, db = utils.connectMongoDB()
+    if readFromMongo:
+        # connecting or switching to the database
+        connection, db = utils.connectMongoDB()
 
-    # creating or switching to ais_navigation collection
-    collection = db.flatten_crypto_data
+        # creating or switching to ais_navigation collection
+        collection = db.flatten_crypto_data
 
-    # Read preprocessed data from mongo
-    res = collection.find()
+        # Read preprocessed data from mongo
+        res = collection.find()
 
-    jsonData = list(res)
+        jsonData = list(res)
 
-    df = pd.DataFrame.from_dict(jsonData)
-    # settings to display all columns
-    pd.set_option("display.max_columns", None)
-    # display the dataframe head
-    df.head(5)
+        df = pd.DataFrame.from_dict(jsonData)
+        # settings to display all columns
+        pd.set_option("display.max_columns", None)
+        # display the dataframe head
+        df.head(5)
+    else:
+        df = pd.read_csv(DATA_FILE_NAME)
 
     # Plot Bitcoin Price from collected data
 
     # Increase size of plot in jupyter
     if doPlots:
         plt.rcParams["figure.figsize"] = (10, 8)
-        plt.plot(df['quote_USD_price'])
+        plt.plot(df['close_1min'])
         plt.xlabel("Observations")
         plt.ylabel("Price (USD)")
         plt.title("Bitcoin price over time")
@@ -126,14 +139,49 @@ if __name__ == '__main__' :
     # Clean Data
     # - Drop id column
     # - Create X df with all predictors except our target price.
-    # - And Y df with target feature ***quote_USD_price***.
+    # - And Y df with target feature ***close_1min***.
 
     # drop _id column
     df.drop(columns=['_id'], inplace=True)
+
+    # drop unnecessary features
+    df.drop(columns=[
+        'high_24h',
+        'last_24h',
+        'unix_timestamp',
+        'bid_24h',
+        'vwap_24h',
+        'volume_24h',
+        'low_24h',
+        'ask_24h',
+        'open_24h',
+        # 'high_1min',
+        # 'volume_1min',
+        # 'low_1min',
+        # 'close_1min',
+        # 'open_1min',
+        'max_supply',
+        'circulating_supply',
+        'total_supply',
+        'quote_USD_price',
+        'quote_volume_24h',
+        'volume_change_24h',
+        'percent_change_1h',
+        'percent_change_24h',
+        'percent_change_7d',
+        'percent_change_30d',
+        'percent_change_60d',
+        'percent_change_90d',
+        'market_cap',
+        'market_cap_dominance',
+        'fully_diluted_market_cap',
+        # 'reddit_compound_polarity'
+    ], inplace=True)
+
     # ensure all data are numeric
     df = df.apply(pd.to_numeric)
     # separate data tou X and y
-    X, y = df.drop(columns=['quote_USD_price']), df['quote_USD_price'].values
+    X, y = df.drop(columns=['close_1min']), df['close_1min'].values
     print(X.shape, y.shape)
 
     """
@@ -158,17 +206,17 @@ if __name__ == '__main__' :
         - Each element o y_series is a univariant time serieas that contains 15 observations 
           of bitcoin price ( target prediction interval 15 min)
     """
-    X_series, y_series = toMultivariateSeries(X_trans, y_trans, 60, 1)
+    X_series, y_series = toMultivariateSeries(X_trans, y_trans, 30, 1)
     print(X_series.shape, y_series.shape)
 
     # Split Train Test Dataset
     # To speed up model designing we will use only 20% of dataset
     # in order to see how LSTM Algorithm works on the Data.
-    cutoff = round(0.90 * len(X_series)) if isDemoMode else 0
-    X_demo_series = X_series[cutoff :]
-    y_demo_series = y_series[cutoff :]
+    cutoff = round(0.95 * len(X_series)) if isDemoMode else 0
+    X_demo_series = X_series[cutoff:]
+    y_demo_series = y_series[cutoff:]
 
-    train_X, train_y, valid_X, valid_y, test_X, test_y = train_test_valid_split(
+    train_X, train_y, valid_X, valid_y, _, _ = train_test_valid_split(
         X_demo_series,
         y_demo_series,
         train_size=0.8,
@@ -177,7 +225,7 @@ if __name__ == '__main__' :
     print("----------------------------")
     print(f"-------- Train X: {train_X.shape}, Train y: {train_y.shape} --------")
     print(f"-------- Valid X: {valid_X.shape}, Valid y: {valid_y.shape} --------")
-    print(f"-------- Test X: {test_X.shape}, Test y: {test_y.shape} --------")
+    # print(f"-------- Test X: {test_X.shape}, Test y: {test_y.shape} --------")
     print("----------------------------\n")
 
     # Network Configurations
@@ -189,21 +237,48 @@ if __name__ == '__main__' :
 
     # Design Network
     # TODO MAKE IT A CLASS.
+    model = Sequential()
+    model.add(LSTM(
+        2 * BATCH_SIZE,
+        return_sequences=True,
+        # activation='relu',
+        # unroll=True,
+        input_shape=(train_X.shape[1], train_X.shape[2]),
+    ))
+    model.add(LSTM(
+        BATCH_SIZE,
+        # activation='relu',
+        # unroll=True,
+    ))
+    model.add(Dense(BATCH_SIZE))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mse')
 
-    model = Sequential([
-        # BatchNormalization(),
-        LSTM(
-            units=100,
-            return_sequences=True,
-            input_shape=(train_X.shape[1], train_X.shape[2])
-        ),
-        Dropout(0.2),
-        LSTM(units=50, return_sequences=True),
-        Dropout(0.2),
-        LSTM(units=50),
-        Dropout(0.2),
-        Dense(units=1, activation="sigmoid")
-    ])
+    # model = Sequential([
+    #     # BatchNormalization(),
+    #     LSTM(
+    #         units=64,
+    #         return_sequences=True,
+    #         unroll=True,
+    #         activation="relu",
+    #         input_shape=(train_X.shape[1], train_X.shape[2])
+    #     ),
+    #     Dropout(0.2),
+    #     LSTM(
+    #         units=50,
+    #         unroll=True,
+    #         activation="relu",
+    #         return_sequences=True
+    #     ),
+    #     Dropout(0.2),
+    #     LSTM(
+    #         units=50,
+    #         unroll=True,
+    #         activation="relu"
+    #     ),
+    #     Dropout(0.2),
+    #     Dense(units=1, activation="relu")
+    # ])
 
     # # model.add(Dropout(0.2))
     # # model.add(LSTM(units=50, return_sequences=True))
@@ -237,7 +312,7 @@ if __name__ == '__main__' :
     checkpoint_dir = "cryptoPrediction/training_demo_1min"
     latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
 
-    if latest_checkpoint is not None :
+    if latest_checkpoint is not None and loadModel:
         # Load the previously saved weights
         model.load_weights(latest_checkpoint)
 
@@ -247,29 +322,30 @@ if __name__ == '__main__' :
         # # display training curves
         # display_training_curves([], loss, 'loss')
 
-    else :
+    else:
         # fit network
         history = model.fit(
             train_X,
             train_y,
             epochs=EPOCHS,
             batch_size=BATCH_SIZE,
-            validation_data=(test_X, test_y),
+            validation_data=(valid_X, valid_y),
             verbose=2,
             shuffle=False,
-            callbacks=[getModelCheckpointCallback(checkpoint_dir+"/cp-{epoch:04d}.ckpt")]  # add the file name and save epoch to file name
+            callbacks=[getModelCheckpointCallback(checkpoint_dir + "/cp-{epoch:04d}.ckpt")] if loadModel else None
+            # add the file name and save epoch to file name
         )
 
         # display training curves
         display_training_curves(history.history['loss'], history.history['val_loss'], 'loss')
 
     # make a prediction
-    y_predict = model.predict(test_X)
+    y_predict = model.predict(valid_X)
     # invert scaling for forecast
     inv_y_predict = scaler.inverse_transform(y_predict)
     # invert scaling for actual
     # test_y = test_y.reshape((len(test_y), 1))
-    inv_y = scaler.inverse_transform(test_y)
+    inv_y = scaler.inverse_transform(valid_y)
     # calculate RMSE
     rmse = sqrt(mean_squared_error(inv_y, inv_y_predict))
     print('Test RMSE: %.3f' % rmse)
